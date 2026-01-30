59dd330f0d90262fd8b4c9c953114c669b785455
ABDULGANI-J <jahangeerabdulgani@gmail.com>
2026-01-31 04:34:27 +0530

    changes added HTML

--- bike/urls.py
+++ bike/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+
+app_name='bike'
+
+
 urlpatterns = [
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
+    path("try/",views.home,name="home")
 ]
--- bike/views.py
+++ bike/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello")
+    return render(request,'index.html')
+def home(request):
+    return HttpResponse("Wellcome")
